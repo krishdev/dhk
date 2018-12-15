e8322173a0493f9f6872f54a47a18a5d06048ae2
Srinidhi Karnati <srinidhikarnati09@gmail.com>
2018-12-15 11:46:39 -0600

    Adding components

---
+++ src/components/profile/profile.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the ProfileComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'profile',
+  templateUrl: 'profile.html'
+})
+export class ProfileComponent {
+
+  text: string;
+
+  constructor() {
+  }
+
+}
