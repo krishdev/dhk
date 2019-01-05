af2168d779c35469d83f5d0948ba8ddade89f335
Srinidhi Karnati <srinidhikarnati09@gmail.com>
2019-01-05 15:52:18 -0600

    Adding search results component

---
+++ src/components/status/status.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the StatusComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'status',
+  templateUrl: 'status.html'
+})
+export class StatusComponent {
+
+  text: string;
+
+  constructor() {
+  }
+
+}
