0580610b3418b2ab209a5157b60df502c97af2d5
Lalitha Vemuri <lalitha@Lalithas-MacBook-Air.local>
2019-01-16 19:13:00 -0600

    setup of orders page

---
+++ src/components/orders/orders.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'orders',
+  templateUrl: 'orders.html'
+})
+export class OrdersComponent {
+
+  text: string;
+
+  constructor() {
+  }
+
+}
